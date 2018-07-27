df463b61115828ba1d5734e70f493719f02f3774
lakshmi76 <41735161+lakshmi76@users.noreply.github.com>
2018-07-27 14:36:01 +0530

    Create yes.java

---
+++ yes.java
@@ -0,0 +1,20 @@
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a;
+	Scanner obj=new Scanner(System.in);
+	a=obj.nextInt();
+	if(a%4==0)
+	{
+		System.out.println("yes");
+		
+	}
+	else
+	{
+		System.out.println("n0");
+		
+	}
+	}
+}
